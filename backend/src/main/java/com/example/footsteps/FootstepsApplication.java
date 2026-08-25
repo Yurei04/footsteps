24211acb82349ff26754346486f95235fe0de2f0
Yuri <142381932+Yurei04@users.noreply.github.com>
2026-08-25 15:25:28 +0800

    backend initialization(maven springboot 21)

---
+++ backend/src/main/java/com/example/footsteps/FootstepsApplication.java
@@ -0,0 +1,13 @@
+package com.example.footsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootstepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootstepsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/footsteps/FootstepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootstepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
